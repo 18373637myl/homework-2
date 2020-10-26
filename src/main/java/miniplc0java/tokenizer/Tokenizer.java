048e3f37e47a4b2c55b5e0b4aa6e7520301c9068
18373637myl <445326510@qq.com>
2020-10-26 16:11:25 +0800

    12

--- src/main/java/miniplc0java/tokenizer/Tokenizer.java
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -50,7 +50,7 @@ public class Tokenizer {
         }
         // -- 前进一个字符，并存储这个字符
         //
-        sum = Integer.parseInt(nowToken.toString());
+        int sum = Integer.parseInt(nowToken.toString());
         // 解析存储的字符串为无符号整数
         return new Token(TokenType.Uint, sum, startpos, it.previousPos());
         // 解析成功则返回无符号整数类型的token，否则返回编译错误
